7f274509a87397fc3e7919337c431f05cda14786
Christine Hoang <christinehoangt@gmail.com>
2024-09-22 12:36:58 -0500

    Add project files.

---
+++ SynthPro/Models/Synth.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SynthPro.Models
+{
+    public class Synth
+    {
+        public int SynthID { get; set; }
+
+        public string ModelNumber { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Price { get; set; }
+
+        public DateTime ReleaseDate { get; set; } = DateTime.Now;
+
+    }
+}
